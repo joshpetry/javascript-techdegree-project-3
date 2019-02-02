81b185790a66dc194cf82120d43f3fa8a30a1db4
Josh Petry <josh.petry@outlook.com>
2019-02-02 07:34:08 -0500

    Update t-shirt info section

--- js/script.js
+++ js/script.js
@@ -17,4 +17,43 @@ $('#title').change( showOtherInput );
 
 
 // "T-Shirt Info" section
-// Hide other title input field by default
+
+
+function checkDesignInput() {
+
+  // Check design value
+  if ( $('#design').val() === 'js puns' ) {
+    $('#color').val('cornflowerblue');
+    $("#color option[value='cornflowerblue']").show();
+    $("#color option[value='darkslategrey']").show();
+    $("#color option[value='gold']").show();
+    $("#color option[value='tomato']").hide();
+    $("#color option[value='steelblue']").hide();
+    $("#color option[value='dimgrey']").hide();
+
+  } else if ( $('#design').val() === 'heart js' ) {
+    $('#color').val('tomato');
+    $("#color option[value='cornflowerblue']").hide();
+    $("#color option[value='darkslategrey']").hide();
+    $("#color option[value='gold']").hide();
+    $("#color option[value='tomato']").show();
+    $("#color option[value='steelblue']").show();
+    $("#color option[value='dimgrey']").show();
+  };
+}
+
+
+$('#design').change( checkDesignInput );
+
+
+/*
+<label for="color">Color:</label>
+<select id="color">
+  <option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
+  <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>
+  <option value="gold">Gold (JS Puns shirt only)</option>
+  <option value="tomato">Tomato (I &#9829; JS shirt only)</option>
+  <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>
+  <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>
+</select>
+*/
