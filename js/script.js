6fb85b7d060c9deae31c9a7d434708d0e542f5e7
Josh Petry <josh.petry@outlook.com>
2019-02-04 19:00:14 -0500

    Refactored t-shirt section
    only did part of it as an example.

--- js/script.js
+++ js/script.js
@@ -19,16 +19,21 @@ $('#title').change( showOtherInput );
 // "T-Shirt Info" section
 // Change color options based on selected design
 function changeColorOptions() {
+
+  function showHide(optionValue, action) {
+    $(`#color option[value='${optionValue}']`)[action]();
+  };
+
   if ( $('#design').val() === 'js puns' ) {
     // Change default color value
     $('#color').val('cornflowerblue');
-    // Show/Hide color options
-    $("#color option[value='cornflowerblue']").show();
-    $("#color option[value='darkslategrey']").show();
-    $("#color option[value='gold']").show();
-    $("#color option[value='tomato']").hide();
-    $("#color option[value='steelblue']").hide();
-    $("#color option[value='dimgrey']").hide();
+    // Show/Hide color options - using the showHide function
+    showHide('cornflowerblue', 'show');
+    showHide('darkslategrey', 'show');
+    showHide('gold', 'show');
+    showHide('tomato', 'hide');
+    showHide('steelblue', 'hide');
+    showHide('dimgrey', 'hide');
   } else if ( $('#design').val() === 'heart js' ) {
     // Change default color value
     $('#color').val('tomato');
@@ -43,16 +48,3 @@ function changeColorOptions() {
 }
 // When design input change, call function to change color options
 $('#design').change( changeColorOptions );
-
-
-/*
-<label for="color">Color:</label>
-<select id="color">
-  <option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
-  <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>
-  <option value="gold">Gold (JS Puns shirt only)</option>
-  <option value="tomato">Tomato (I &#9829; JS shirt only)</option>
-  <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>
-  <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>
-</select>
-*/
